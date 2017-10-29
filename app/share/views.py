a14b7991d4c1de24ec5b23b97b2341bff686ccfe
lubalesuan <lubavable@mail.ru>
2017-10-28 21:41:10 -0400

    create sidebar and menu

--- app/share/views.py
+++ app/share/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Project, CustomUser, Categories
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-	return HttpResponse("hello world")
+class IndexView(generic.ListView):
+	template_name = 'share/index.html'
+	def get_queryset(self):
+		return Project.objects.filter(
+			publish_date__lte = timezone.now()
+		).order_by('-publish_date')
+
+	
