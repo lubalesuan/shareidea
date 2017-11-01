972a8588b247a0d2d36cf08719491af33e457114
sfasone <sfasone@gmail.com>
2017-11-01 17:09:09 -0400

    Added ModelMultipleChoiceField to search form

--- app/share/forms.py
+++ app/share/forms.py
@@ -3,4 +3,5 @@ from django import forms
 class SearchForm(forms.Form):
     input_name = forms.CharField(label='Project name', max_length=100, required = False)
     input_date = forms.DateField(label='Creation date', required = False)
-    input_category = forms.CharField(label='Category', max_length=100, required = False)
+    # input_category = forms.CharField(label='Category', max_length=100, required = False)
+    input_category = forms.ModelMultipleChoiceField(label='Category')
