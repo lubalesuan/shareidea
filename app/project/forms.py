0a99deea8fc7fdfa6912dd77634df31fecddc866
lubalesuan <lubavable@mail.ru>
2017-11-26 23:25:40 -0500

    wow I found the great BEAUTY

--- app/project/forms.py
+++ app/project/forms.py
@@ -6,10 +6,10 @@ from .models import Category, Project
 from .models import Category
 
 class SearchForm(forms.Form):
-    input_name = forms.CharField(label='Project', max_length=20, required = False)
-    input_min_date = forms.DateField(label='Earliest', required = False,widget=TextInput(attrs={'type': 'date'}) )
-    input_max_date = forms.DateField(label='Latest', required = False,widget=TextInput(attrs={'type': 'date'}) )
-    input_category = forms.ModelMultipleChoiceField(label='Category', queryset = Category.objects.all().order_by(
+    pn = forms.CharField(label='Project', max_length=20, required = False)
+    psd = forms.DateField(label='Earliest', required = False,widget=TextInput(attrs={'type': 'date'}) )
+    pgd = forms.DateField(label='Latest', required = False,widget=TextInput(attrs={'type': 'date'}) )
+    cat = forms.ModelMultipleChoiceField(label='Category', queryset = Category.objects.all().order_by(
     	'category_name'), required=False)
 
 class CreateProjectForm(ModelForm):
--- app/project/views.py
+++ app/project/views.py
@@ -1,60 +1,72 @@
-from django.shortcuts import get_object_or_404, render,redirect
-from .models import Project, Category
-from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render,redirect
 from django.urls import reverse
-from django.views import generic
-from django.utils import timezone
-import datetime
+from django.utils import timezone, http
 
 from .forms import SearchForm, CreateProjectForm
+from .models import Project, Category
 
+# view for index page
 def index(request):
-	print('in index')
-	results = Project.objects.all()
 
-	if request.method == 'POST':
-		print('post')
-		name = request.POST.get('input_name', None)
-		if name:
-			results = results.filter(project_name__icontains = name)
-			print('name: ',results)
+	#search dispatcher
+	def search(query_set):
+		results = Project.objects.all()
+		query_set = dict(query_set)
+		# searches is a dictionary of search functions
+		searches = {
+			# project name
+			'pn': lambda x: results.filter(project_name__icontains = x),
+			# earliest post date for project
+			'psd': lambda x: results.filter(publish_date__gte = x),
+			# latest post date for project
+			'pgd': lambda x: results.filter(publish_date__lte = x),
+			# project category
+			'cat':lambda x: results.filter(category__in = eval(x)),
+		}
+		# if URL contains illgeal keys, filter them out
+		key_set = set(query_set) & set(searches)
+		# for each search parameter call corresponding search function
+		for key in key_set:
+			results = searches [key] (query_set[key].pop())
+		return results
 
-		min_date = request.POST.get('input_min_date', None)
-		if min_date:
-			results = results.filter(publish_date__gte = min_date)
-			print('min_date: ',results)
+	# creating search form for the index page
+	form = SearchForm(request.POST or None)
+ 	
+	if request.method == 'POST' and form.is_valid():
+		# data is a dictionary of search parameters
+		data = {}
+		for key in form.cleaned_data:
+			# because 'cat' form field is a multiselect, put the choices in a list
+			if form.cleaned_data['cat']:
+				data['cat'] = request.POST.getlist('cat', None)
+			# if a field was filled by user, put it in dictionary
+			elif form.cleaned_data[key]:
+				data[key] = form.cleaned_data[key]
+		# pass dictionary as a query string to GET request
+		q = http.urlencode(data)
+		return redirect(reverse(index)+'?'+q)
+	# render index page, pass search form to template, pass list of results from search to template
+	return render(request,'project/index.html', {'form':form, 'project_list': search(request.GET)})
 
-		max_date = request.POST.get('input_max_date', None)
-		if max_date:
-			results = results.filter(publish_date__lte = max_date)
-			print('max_date: ',results)
-
-		category = request.POST.getlist('input_category', None)
-		if category:
-			results = results.filter(category__in = category)
-			print('cat:',category)
-			print('category: ',request.POST['input_category'])
-
-		return render(request,'project/index.html', {'form':  SearchForm(request.POST), 'project_list': results})
-
-	return render(request,'project/index.html', {'form': SearchForm(),'project_list': results})
-	
+# renders project page, url ends with project id
 def projectPage(request, pk = 0):
 	pk = int(pk)
-	# create = pk == 0
-	# if not create:
+	# get project by id
 	project = Project.objects.get(pk = pk)
-	# else:
+	# render project page, pass project object to template
 	return render(request, 'project/projectpage.html', {'project': project} )
 
+# renders project creation page
 def createProject(request):
 	if request.method == 'POST':
 		form = CreateProjectForm(request.POST)
 		if form.is_valid():
-			p = form.save()
-			
-		return redirect('project_list')
-
+			# save form to database
+			p = form.save()	
+			# redirect to index page
+			return redirect('project_list')
+	# if form is invalid or other requests, rerender project creation page
 	return render(request,'project/create_project.html',{'form': CreateProjectForm()})
 
 	
